3e33ee56543efb106de02933f8800e2e7ded6737
dajminazwe <dajminazwe@gmail.com>
2017-05-29 19:54:36 +0200

    Created request and response models.

--- src/main/java/com/holiday/model/HolidayApiRequest.java
+++ src/main/java/com/holiday/model/HolidayApiRequest.java
@@ -1,4 +1,21 @@
 package com.holiday.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class HolidayApiRequest {
+
+    private String firstCountryCode;
+
+    private String secondCountryCode;
+
+    private Date date;
 }
--- src/main/java/com/holiday/model/HolidayApiResponse.java
+++ src/main/java/com/holiday/model/HolidayApiResponse.java
@@ -1,4 +1,21 @@
 package com.holiday.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HolidayApiResponse {
+
+    private Date date;
+
+    private String firstHoliday;
+
+    private String secondHoliday;
 }
